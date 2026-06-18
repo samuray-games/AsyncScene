b081cc2eea14f9ae941ae8404d862d27b608d792
samuray-games <raykhalit@icloud.com>
2026-06-18 14:29:58 +0900

    .

--- AsyncScene/Web/dev/dev-checks.js
+++ AsyncScene/Web/dev/dev-checks.js
@@ -16532,7 +16532,7 @@ NF_0043 | action_honesty | TXT_0058 | before "Ставка списывает р
 
   if (!DEV_FLAG) return;
 
-  const devStore = ensureDevStoreSurface();
+  var devStore = ensureDevStoreSurface();
   addStage3SmokeHelper(devStore);
   addRespectLedgerSmokeHelper(devStore);
   addRespectEmitterSmokeHelper(devStore);
